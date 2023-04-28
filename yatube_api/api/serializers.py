c09fe6be4de24347d9ab2d5017d2a5f6af5ca62a
Альбина Гильмутдинова <inovaras@mail.ru>
2023-04-28 22:15:38 +0500

    refactor2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,7 +1,7 @@
 import base64
 from django.core.files.base import ContentFile
-from rest_framework.relations import SlugRelatedField
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
 
 from posts.models import Comment, Group, Follow, Post, User
